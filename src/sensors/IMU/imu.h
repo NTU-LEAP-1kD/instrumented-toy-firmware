e04950c43aa8ec657af3a785d7f611e472c55186
Victor Gwee <47884004+vicgwee@users.noreply.github.com>
2021-08-27 10:36:33 +0800

    [WIP] Trying out code from https://github.com/sparkfun/OpenLog_Artemis/blob/6d8b97cb62ce70eb356a97493878fc339d12c8df/Firmware/Test%20Sketches/OLA_IMU_Basics/OLA_IMU_Basics.ino

---
+++ src/sensors/IMU/imu.h
@@ -0,0 +1,5 @@
+#pragma once 
+
+void imuPowerOn();
+void imuPowerOff();
+bool enableCIPOpullUp();
---
+++ src/sensors/IMU/imuhelpers.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include "config/config.h"
+
+void imuPowerOn()
+{
+  pinMode(PIN_IMU_POWER, OUTPUT);
+  digitalWrite(PIN_IMU_POWER, HIGH);
+}
+void imuPowerOff()
+{
+  pinMode(PIN_IMU_POWER, OUTPUT);
+  digitalWrite(PIN_IMU_POWER, LOW);
+}
+
+#if defined(ARDUINO_ARCH_MBED) // updated for v2.1.0 of the Apollo3 core
+bool enableCIPOpullUp()
+{
+  //Add 1K5 pull-up on CIPO
+  am_hal_gpio_pincfg_t cipoPinCfg = g_AM_BSP_GPIO_IOM0_MISO;
+  cipoPinCfg.ePullup = AM_HAL_GPIO_PIN_PULLUP_1_5K;
+  pin_config(PinName(PIN_SPI_CIPO), cipoPinCfg);
+  return (true);
+}
+#else
+bool enableCIPOpullUp()
+{
+  //Add CIPO pull-up
+  ap3_err_t retval = AP3_OK;
+  am_hal_gpio_pincfg_t cipoPinCfg = AP3_GPIO_DEFAULT_PINCFG;
+  cipoPinCfg.uFuncSel = AM_HAL_PIN_6_M0MISO;
+  cipoPinCfg.ePullup = AM_HAL_GPIO_PIN_PULLUP_1_5K;
+  cipoPinCfg.eDriveStrength = AM_HAL_GPIO_PIN_DRIVESTRENGTH_12MA;
+  cipoPinCfg.eGPOutcfg = AM_HAL_GPIO_PIN_OUTCFG_PUSHPULL;
+  cipoPinCfg.uIOMnum = AP3_SPI_IOM;
+  padMode(MISO, cipoPinCfg, &retval);
+  return (retval == AP3_OK);
+}
+#endif
