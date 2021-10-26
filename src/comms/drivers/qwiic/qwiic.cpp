eae0c23ea2108f1de7e20902ffa68f9ff63a4d66
Victor Gwee <47884004+vicgwee@users.noreply.github.com>
2021-10-26 17:56:56 +0800

    3/feat/qwiic setup (#14)
    * Setup qwiic
    
    * init Qwiic first

---
+++ src/comms/drivers/qwiic/qwiic.cpp
@@ -0,0 +1,70 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "config/config.h"
+#include "qwiic.h"
+
+TwoWire qwiic(PIN_QWIIC_SDA,PIN_QWIIC_SCL); //Will use pads 8/9
+
+void initQwiic(){
+    qwiicPowerOn();
+    delay(settings.qwiicBusPowerUpDelayMs);
+    qwiic.begin();
+    qwiic.setClock(settings.qwiicBusMaxSpeed);
+    setQwiicPullups(settings.qwiicBusPullUps);
+}
+
+//From https://github.com/sparkfun/OpenLog_Artemis/blob/main/Firmware/Test%20Sketches/Sensor_Autodetect/I2CScanner/I2CScanner.ino
+void qwiicPowerOn()
+{
+  pinMode(PIN_QWIIC_POWER, OUTPUT);
+#if(HARDWARE_VERSION_MAJOR == 0)
+  digitalWrite(PIN_QWIIC_POWER, LOW);
+#else
+  digitalWrite(PIN_QWIIC_POWER, HIGH);
+#endif
+}
+void qwiicPowerOff()
+{
+  pinMode(PIN_QWIIC_POWER, OUTPUT);
+#if(HARDWARE_VERSION_MAJOR == 0)
+  digitalWrite(PIN_QWIIC_POWER, HIGH);
+#else
+  digitalWrite(PIN_QWIIC_POWER, LOW);
+#endif
+}
+
+void setQwiicPullups(uint32_t qwiicBusPullUps)
+{
+  //Change SCL and SDA pull-ups manually using pin_config
+  am_hal_gpio_pincfg_t sclPinCfg = g_AM_BSP_GPIO_IOM1_SCL;
+  am_hal_gpio_pincfg_t sdaPinCfg = g_AM_BSP_GPIO_IOM1_SDA;
+
+  if (qwiicBusPullUps == 0)
+  {
+    sclPinCfg.ePullup = AM_HAL_GPIO_PIN_PULLUP_NONE; // No pull-ups
+    sdaPinCfg.ePullup = AM_HAL_GPIO_PIN_PULLUP_NONE;
+  }
+  else if (qwiicBusPullUps == 1)
+  {
+    sclPinCfg.ePullup = AM_HAL_GPIO_PIN_PULLUP_1_5K; // Use 1K5 pull-ups
+    sdaPinCfg.ePullup = AM_HAL_GPIO_PIN_PULLUP_1_5K;
+  }
+  else if (qwiicBusPullUps == 6)
+  {
+    sclPinCfg.ePullup = AM_HAL_GPIO_PIN_PULLUP_6K; // Use 6K pull-ups
+    sdaPinCfg.ePullup = AM_HAL_GPIO_PIN_PULLUP_6K;
+  }
+  else if (qwiicBusPullUps == 12)
+  {
+    sclPinCfg.ePullup = AM_HAL_GPIO_PIN_PULLUP_12K; // Use 12K pull-ups
+    sdaPinCfg.ePullup = AM_HAL_GPIO_PIN_PULLUP_12K;
+  }
+  else
+  {
+    sclPinCfg.ePullup = AM_HAL_GPIO_PIN_PULLUP_24K; // Use 24K pull-ups
+    sdaPinCfg.ePullup = AM_HAL_GPIO_PIN_PULLUP_24K;
+  }
+
+  pin_config(PinName(PIN_QWIIC_SCL), sclPinCfg);
+  pin_config(PinName(PIN_QWIIC_SDA), sdaPinCfg);
+}
